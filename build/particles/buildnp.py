7f75513c7cbd0386996c91c201088a8a620ccaeb
Nathan Horst <nathan@hyzer.ameslab.iastate.edu>
2016-06-20 09:34:09 -0500

    changes 6-20-2016

--- build/particles/buildnp.py
+++ build/particles/buildnp.py
@@ -19,17 +19,26 @@ uc_bcc = npc.NanoBcc(uc_size)
 searchquery1='C'
 vec_all=[]
 
-with open('basefcc.xyz') as f1:
+with open('fcc_large.xyz') as f1:
     lines=f1.readlines()
     for i, line in enumerate(lines):
         if line.startswith(searchquery1):
             split=str(line).split()
             vec_all.append(np.array([split[1],split[2],split[3]]).astype(np.float))
 type = 'C'
+pos= np.array([[-100.0,0.0,0.0]])
+count=0
 with open("fccnp_cutbcc.xyz",'w') as f3:    
     for ind, vec in enumerate(vec_all):
         if uc_bcc.check_point(np.array(vec)):
             print(vec)
-            f3.write('%s %1.6f %1.6f %1.6f'% (type, vec[0], vec[1], vec[2])+'\n')
+            pos=np.append(pos,[[vec[0], vec[1], vec[2]]], axis=0)
+            count+=1
+    f3.write(('%s \n\n')%(str(count)))
+    for i in range(1,count+1):
+        if(pos[i][0]==0.0 and pos[i][1]==0.0 and pos[i][2]==0.0):
+            f3.write('%s %1.6f %1.6f %1.6f'% ('V', pos[i][0], pos[i][1], pos[i][2])+'\n')
+        else:
+            f3.write('%s %1.6f %1.6f %1.6f'% (type, pos[i][0], pos[i][1], pos[i][2])+'\n')
 f3.close()
             
