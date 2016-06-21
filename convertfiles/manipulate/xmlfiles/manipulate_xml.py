0977d79b57c34fffb147d0e54f1427dc2c1f4f4c
Nathan Horst <nathan@hyzer.ameslab.iastate.edu>
2016-06-21 10:54:10 -0500

    new manipulate xml files

--- convertfiles/manipulate/xmlfiles/manipulate_xml.py
+++ convertfiles/manipulate/xmlfiles/manipulate_xml.py
@@ -22,7 +22,8 @@ def rename_particles_xml(save,read,newname,oldname):
     data=fdata.splitlines()
 
     for line in data:
-        if line.startswith(oldname):    
+        s=line.split()
+        if s[0]==oldname:    
             fid.write(('%s \n')%(newname))
         else:
             fid.write(('%s \n')%(line))
@@ -42,6 +43,25 @@ def rename_bonds_xml(save,read,newname,oldname):
         if line.startswith(oldname):
             s=line.split()
             fid.write(('%s %s %s \n')%(newname,s[1],s[2]))
+        else:
+            fid.write(('%s \n')%(line))
+            
+            
+######################################################################
+### This function simply copies an xml file, and renames the dihedrals
+######################################################################
+
+def rename_dihedrals_xml(save,read,newname,oldname):
+    
+    fid = open(save,'w')
+    inputfile = open(read,'r')
+    fdata=inputfile.read()
+    data=fdata.splitlines()
+
+    for line in data:
+        if line.startswith(oldname):
+            s=line.split()
+            fid.write(('%s %s %s %s\n')%(newname,s[1],s[2],s[3]))
         else:
             fid.write(('%s \n')%(line))
 
@@ -67,9 +87,7 @@ def rename_angles_xml(save,read,newname,oldname):
 ### This function shifts the positions of xyz files in the x y or z directions
 ##############################################################################
 
-#### THIS FUNCTION DOES NOT CURRENTLY WORK AND IS NOT FINISHED
-
-def shift_xml(save,read,shiftx,shifty,shiftz,npart):
+def shift_xml(save,read,shiftx,shifty,shiftz):
 
 
     #Open input file for reading in xml format
@@ -81,16 +99,54 @@ def shift_xml(save,read,shiftx,shifty,shiftz,npart):
     #Open output file for writing in xml format
 
     fout=open(save,'w')
-
+    
+    writer=True
     for line in data:
-        s=line.split()
-        if line.startswith('<position>'):
-            for line, i in range(npart-1):
-                x=float(s[0])+shiftx
-                y=float(s[1])+shifty
-                z=float(s[2])+shiftz
-                fout.write(("%s %f %f %f\n")%(x,y,z))
+        if writer==True:
+            fout.write(('%s \n')%(line))
+        if line.startswith('<position'):
+            writer=False
+        if writer==False and not(line.startswith('<position') or line.startswith('</position')):              
+            s=line.split()
+            x=float(s[0])+shiftx
+            y=float(s[1])+shifty
+            z=float(s[2])+shiftz
+            fout.write(("%f %f %f\n")%(x,y,z))
+        if line.startswith('</position'):
+            writer=True
+            
+##############################################################################
+### This function scales the positions of xyz files in the x y or z directions
+##############################################################################           
 
+def scale_xml(save,read,scalex,scaley,scalez):
+
+
+    #Open input file for reading in xml format
+
+    fin=open(read,'r')
+    fdata=fin.read()
+    data=fdata.splitlines()
+
+    #Open output file for writing in xml format
+
+    fout=open(save,'w')
+    
+    writer=True
+    for line in data:
+        if writer==True:
+            fout.write(('%s \n')%(line))
+        if line.startswith('<position'):
+            writer=False
+        if writer==False and not(line.startswith('<position') or line.startswith('</position')):              
+            s=line.split()
+            x=float(s[0])*scalex
+            y=float(s[1])*scaley
+            z=float(s[2])*scalez
+            fout.write(("%f %f %f\n")%(x,y,z))
+        if line.startswith('</position'):
+            writer=True
+            
 ##########################
 ### Example run script ###
 ##########################
